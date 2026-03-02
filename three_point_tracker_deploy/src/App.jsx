ce4c55749cf0b9539d55b253e132881a89007d4e
TallBaller22 <stephen@hurleyonline.us>
2026-03-02 14:49:04 -0700

    Delete three_point_tracker_deploy directory

--- three_point_tracker_deploy/src/App.jsx
+++
@@ -1,6 +0,0 @@
-
-// Placeholder wrapper. Replace with your full component from Canvas.
-import React from 'react'
-export default function App(){
-  return (<div style={{padding:20}}>Your full tracker code goes here.</div>)
-}
